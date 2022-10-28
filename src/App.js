594a6fc617dbf17317a6452a9ff550dfa5c4341c
afjaguarking <af.jaguarking@gmail.com>
2022-10-28 16:39:45 +0900

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,95 +1,61 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { a } from 'react-router-dom';
 import image_1 from './assets/images/1.jpg'
 import image_2 from './assets/images/2.jpg'
 import image_4 from './assets/images/4.jpg'
 
 // import $ from 'jquery';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-      isClose:false
-    };
-  }
-  openNav() {
-    this.setState({...this.state,isClose:false});
-  }
+const App = () => {
+  console.log('dddd')
+  const[isClose,setClose] = useState(true);
   
-  closeNav() {
-    this.setState({...this.state,isClose:true});
-    
+  const toggle = () => {
+    setClose(!isClose)
+
   }
-  render(){ 
-    return (
-      <div className="App">
-        <nav>
-          <ul className="topnav">
-            <li className="left">
-
-
-
-
-
-
-
-
-
-
-
-
-              
-              <img className='logo' src='./logo4.png'></img>
-            </li>
-            <li className='n'><a href="#contact">Contact</a></li> 
-            <li className='n'><a href="#about">About</a></li>
-            <li className='n'><a href="#news">Services</a></li>
-            <li className='n'><a href="#home">Home</a></li>
-            <li className="right">
-              <button id="openbtn" onClick={() => this.openNav()}>&#9776;</button>
-            </li>
-            dsfdsfdsfsd
-            dsfdsfdsfsdf
-            dsfdsfdsfsdf
-          </ul>
-          <div id="Sidenav" className={this.state.isClose?'sidenav hidden':'sidenav'}>
-            <a href="#" className="closebtn" onClick={() => this.closeNav()}>&times;</a>
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-          </div>
-        </nav>
-
-
-
-
-
-        <div className='flex column minH-200 p-10'>
-          <div className='w-100 minH-50 pt-50'>
-            <img src={image_1} alt='' />  
-          </div>
-        
-          <div className='flex w-100 justify-content-center' id="twoimg">
-            <img className='img2' src={image_2} alt='img2' />
-            <img className='img4' src={image_4} alt='img4' />
-          </div>
+  return (
+    <div className="App">
+      <nav>
+        <ul className="topnav">
+          <li className="left">
+            <img className='logo' src='./logo4.png' alt='logo'></img>
+          </li>
+          <li className='n'><a to="#contact">Contact</a></li> 
+          <li className='n'><a to="#about">About</a></li>
+          <li className='n'><a to="#news">Services</a></li>
+          <li className='n'><a to="#home">Home</a></li>
+          <li className="right">
+            <button id="openbtn" onClick={() => toggle()}>&#9776;</button>
+          </li>
+        </ul>
+        <div id="Sidenav" className={isClose ?'sidenav hidden':'sidenav'}>
+          <a to={'/'} className="closebtn" onClick={() => toggle()}>&times;</a>
+          <a to={'/'}>Homes</a>
+          <a to={'/'}>Clients</a>
+          <a to={'/'}>Contact</a>
         </div>
-
-        <div id='footer'>
-          
+      </nav>
+      <div className='flex column minH-200 p-10'>
+        <div className='w-100 minH-50 pt-50'>
+          <img src={image_1} alt='img1' />  
         </div>
-
-        <div className={this.state.isClose?'hover-drop hidden':'hover-drop'}  onClick={this.closeNav()}>
-
+        <div className='flex w-100 justify-content-center' id="twoimg">
+          <img className='img2' src={image_2} alt='img2' />
+          <img className='img4' src={image_4} alt='img4' />
         </div>
       </div>
-    );
-  }
-  
+
+      <div id='footer'>
+        
+      </div>
+
+      <div className={isClose ? 'hover-drop hidden' : 'hover-drop'}  onClick={() => toggle()}></div>
+    </div>
+  );
 }
+
 
 export default App;
